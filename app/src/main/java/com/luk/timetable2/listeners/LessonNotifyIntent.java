413bb66f43fbc341db6de6f3785fb8b590c9ed16
LuK1337 <priv.luk@gmail.com>
2015-10-01 23:56:36 +0200

    Make lessons notifier optional

--- app/src/main/java/com/luk/timetable2/listeners/LessonNotifyIntent.java
+++ app/src/main/java/com/luk/timetable2/listeners/LessonNotifyIntent.java
@@ -3,7 +3,9 @@ package com.luk.timetable2.listeners;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Vibrator;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.luk.timetable2.Utils;
@@ -18,8 +20,12 @@ public class LessonNotifyIntent extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        Vibrator v = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
-        v.vibrate(200);
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        if (sharedPref.getBoolean("notifications_vibrate", false)) {
+            Vibrator v = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(200);
+        }
 
         LessonNotifyWakeReceiver.completeWakefulIntent(intent);
     }

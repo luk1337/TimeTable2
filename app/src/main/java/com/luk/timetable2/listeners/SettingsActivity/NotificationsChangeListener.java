9de42f416c255f5c22ea371dd957e9ebb6f374ad
LuK1337 <priv.luk@gmail.com>
2015-10-06 18:44:01 +0200

    NotificationsChangeListener: Do not use recreate()

--- app/src/main/java/com/luk/timetable2/listeners/SettingsActivity/NotificationsChangeListener.java
+++ app/src/main/java/com/luk/timetable2/listeners/SettingsActivity/NotificationsChangeListener.java
@@ -3,6 +3,7 @@ package com.luk.timetable2.listeners.SettingsActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.preference.Preference;
+import android.preference.PreferenceManager;
 
 import com.luk.timetable2.services.RegisterReceivers;
 
@@ -18,8 +19,12 @@ public class NotificationsChangeListener implements Preference.OnPreferenceChang
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean isEnabled = (boolean) newValue;
+        PreferenceManager preferenceManager = preference.getPreferenceManager();
+
+        preferenceManager.findPreference("notifications_vibrate_length").setEnabled(isEnabled);
+        preferenceManager.findPreference("notifications_vibrate_time").setEnabled(isEnabled);
         mActivity.sendBroadcast(new Intent(mActivity, RegisterReceivers.class));
-        mActivity.recreate();
 
         return true;
     }
